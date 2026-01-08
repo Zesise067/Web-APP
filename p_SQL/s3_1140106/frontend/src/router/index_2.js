3150d6baf6b73866c1e660758331e0d1b97318b3
Zesise067 <zesisetest@gmail.com>
2026-01-08 21:43:19 +0800

    🟢 Step 3-2-7把「庫存異動紀錄頁」加到首頁選單

--- p_SQL/s3_1140106/frontend/src/router/index_2.js
+++ p_SQL/s3_1140106/frontend/src/router/index_2.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProductList from '../views/ProductList.vue'
 import AddProduct from '../views/AddProduct.vue'
-import StockLogs from '../views/StockLogs.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: ProductList },
-    { path: '/add', component: AddProduct },
-    { path: '/logs', component: StockLogs }
+    { path: '/add', component: AddProduct }
   ]
 })
 
