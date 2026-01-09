5057cbd198374f8411ad5b92d2fb39b28838c2f2
Zesise067 <zesisetest@gmail.com>
2026-01-09 22:08:51 +0800

    Last editd: 1150109

---
+++ p_SQL/s4_1150109/vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  base: '/GitHub-Pages/',
+  build: {
+    outDir: 'build' // 這裡如果被改成 'build'，資料夾名稱就會是 build 而非 dist
+  }
+})
+
